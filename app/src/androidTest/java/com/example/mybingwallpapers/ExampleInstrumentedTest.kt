56053fe5cf8210419035ba96c72c3cc1eec96bda
olga <korotkovaov@gmail.com>
2021-04-14 10:37:19 +0200

    test skeleton added (not much help for now)

--- app/src/androidTest/java/com/example/mybingwallpapers/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/mybingwallpapers/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package com.example.mybingwallpapers
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.work.ListenableWorker
+import androidx.work.testing.TestWorkerBuilder
+import org.hamcrest.CoreMatchers.`is`
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +28,29 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.mybingwallpapers", appContext.packageName)
+    }
+}
+
+// Kotlin code uses the TestWorkerBuilder extension to build the Worker
+class GetImageWorkerTest {
+    private lateinit var context: Context
+    private lateinit var executor: Executor
+
+    @Before
+    fun setUp() {
+        context = ApplicationProvider.getApplicationContext()
+        executor = Executors.newSingleThreadExecutor()
+    }
+
+    @Test
+    fun testGetImageWorker() {
+        val worker = TestWorkerBuilder<GetImageWorker>(
+            context = context,
+            executor = executor
+        ).build()
+
+        val result = worker.doWork()
+        assertThat(result, `is`(ListenableWorker.Result.success()))
+
     }
 }
