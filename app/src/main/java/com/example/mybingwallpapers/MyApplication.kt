c8792960cd84be91888d956157295385eda8e757
olga <korotkovaov@gmail.com>
2021-04-14 10:55:18 +0200

    increased WorkManager's own logging level

--- app/src/main/java/com/example/mybingwallpapers/MyApplication.kt
+++ app/src/main/java/com/example/mybingwallpapers/MyApplication.kt
@@ -2,12 +2,19 @@ package com.example.mybingwallpapers
 
 import android.app.Application
 import timber.log.Timber
+import androidx.work.Configuration
 
-class MyApplication : Application() {
+class MyApplication : Application(), Configuration.Provider {
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         Timber.plant(FileLoggingTree(applicationContext))
     }
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setMinimumLoggingLevel(android.util.Log.INFO)
+            .build()
+
 }
